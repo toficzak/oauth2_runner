ec06ef43f76399e4bd086dfcb9bcf72507303dff
Toficzak <toficzak@duck.com>
2023-09-25 21:47:58 +0200

    A bit more structure

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 from flask import Flask, request
 import requests
 import webbrowser
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
 @app.route("/login", methods=['GET'])
 def init_login():
-    webbrowser.open('http://localhost:8080/realms/test/protocol/openid-connect/auth?response_type=code&scope=email&client_id=test&redirect_uri=http://localhost:5000/callback', new=2)
+    idp_url = os.environ['ms_idp_url']
+    client_id = os.environ['ms_client_id']
+    callback_url = os.environ['ms_redirect_uri']
+    url = idp_url + ('/realms/test/protocol/openid-connect/auth'
+                     '?response_type=code'
+                     '&scope=email' +
+                     '&client_id=' + client_id +
+                     '&redirect_uri=' + callback_url)
+    webbrowser.open(url, new=2)
     return "ok"
+
 
 @app.route("/callback", methods=['GET'])
 def input_access_token():
+    client_id = os.environ['ms_client_id']
+    client_secret = os.environ['ms_client_secret']
+    redirect_uri = os.environ['ms_redirect_uri']
+
     args = request.args
     print(args)
     code = args.to_dict()["code"]
@@ -23,14 +34,17 @@ def input_access_token():
 
     params = {
         "grant_type": "authorization_code",
-        "client_id": "test",
-        "client_secret":"6kQHYQGUWTj7W41awLXFxNqgUkGpFo4h",
-        "redirect_uri": "http://localhost:5000/callback"
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "redirect_uri": redirect_uri
     }
 
     params["code"] = code
 
-    resp = requests.post("http://localhost:8080/realms/test/protocol/openid-connect/token", data=params, headers=headers)
+    idp_url = os.environ['ms_idp_url']
+    idp_realm = os.environ['ms_idp_realm']
+    url = idp_url + "/realms/" + idp_realm + "/protocol/openid-connect/token"
+    resp = requests.post(url, data=params, headers=headers)
     access_token = resp.json()["access_token"]
     print(access_token)
     return access_token
---
+++ run_example.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ms_idp_url=
+export ms_app_url=
+export ms_client_id=
+export ms_client_secret=
+export ms_redirect_uri=
+export ms_idp_realm=
+
+flask run
